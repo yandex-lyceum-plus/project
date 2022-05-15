f71e0c7aaff08c4014f0acc0eabfd04c44843022
Alexey <alexisv.shishkin@gmail.com>
2022-05-15 17:31:33 +0700

    auntifications

--- proj/users/urls.py
+++ proj/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from django.http import HttpResponse
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from users.views import signup, profile, MyLoginForm
+
 
 urlpatterns = [
-    path('', lambda request: HttpResponse('Users app'))
+    path('login/', LoginView.as_view(template_name='login.html', authentication_form=MyLoginForm), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('signup/', signup, name='signup'),
+    path('profile/', profile, name='profile'),
 ]
